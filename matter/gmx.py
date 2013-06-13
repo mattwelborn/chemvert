dac9cd690587ec79b5f2dfd773f076918f123375
= <welborn@mit.edu>
2013-06-13 16:10:49 -0400

    gro read

--- matter/gmx.py
+++ matter/gmx.py
@@ -2,9 +2,10 @@ import atom
 import molecule
 
 class GMXAtom(atom.Atom):
-    def __init__(self, symbol, name, number, mass, r, gmxname):
+    def __init__(self, symbol, name, number, mass, r, gmxname, v):
         atom.Atom.__init__(self, symbol, name, number, mass, r)
         self.gmxname = gmxname
+        self.v = v
 
 
 class GMXMolecule(molecule.Molecule):
